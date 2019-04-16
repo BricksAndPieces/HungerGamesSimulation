53b56b2183cf9d6f40801fea83273eeaf31842ed
Wicked <90306750@ep-student.org>
2019-04-15 20:27:54 -0500

    box updated

--- src/main/java/hungergames/userinterface/Box.java
+++ src/main/java/hungergames/userinterface/Box.java
@@ -1,14 +1,17 @@
-package hungergames.userinterface;
+package userinterface;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Box extends JFrame  {
+public class Box extends JFrame implements ActionListener{
 
     public static final long serialVersionUID = 1L; //Don't touch
 
     //Vars
+    private boolean isResizable = false; //Resize Box?
+    private int[] size = {600,600}; //Size of box
 
     //Items
     JTextField Username = new JTextField();
@@ -19,22 +22,33 @@ public class Box extends JFrame  {
         new Box().setVisible(true);
     }
 
-    private Box(){
+    private Box() {
         //Box itself
-        super("Hunger Games Simulation");
+        super("Hunger Games Simulation"); // Title
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(600,600);
-        setResizable(false);
+        setSize(size[0], size[1]);
+        setResizable(isResizable);
         setLayout(new FlowLayout()); //BorderLayout
 
+        //Give action listeners
+        button.addActionListener(this);
+
         //Username textfield
-        Username.setSize(100,100);
+        Username.setSize(100, 100);
 
         //Adds - puts it on the box
         add(submitCheck);
         add(Username);
         add(button);
 
+    }
+
+    //Actions
+    public void actionPerformed(ActionEvent e){
+        String name = e.getActionCommand();
+        if(name.equals("Submit")){
+            submitCheck.setText(Username.getText());
+        }
     }
 
 }
